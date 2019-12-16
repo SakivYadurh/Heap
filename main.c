bf9b65d8e8074cc0f629a39fc0ca1464a48a2d78
SakivYadurh <58883626+SakivYadurh@users.noreply.github.com>
2019-12-16 19:00:51 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,42 @@
+#include<stdio.h>
+#include<string.h>
 
+int count(FILE *fp){
+    char c;
+    int f;
+    while((c=getc(fp))!=EOF){
+          f++;
+    }
+    return f;
+}
+void write(FILE *fp){
+     int c;
+     while((c=getchar()!='\n'){
+           putc(c,fp);
+     }
+}
+int main(){
+    int c;
+    char p1[100],p2[100],f1[100];
+    FILE *fp;
+    p1={'p','a','s','s','c','r','y','p','t'};
+    p2={'d','e','w','o','r','d'};
+    printf("enter filename:");
+    gets(f1);
+    if(!strcmp(f1,"file1"))
+       fp=fopen(f1,"r");
+    else if(!strcmp(f1,"file2"))
+       fp=fopen(f1,"r");
+    else if(!strcmp(f1,"file3"))
+       fp=fopen(f1,"r");
+    else if(!strcmp(f1,"file4"))
+       fp=fopen(f1,"r");
+    else 
+       printf("File doesn't exist.\nEnter a valid filename.");
+    c=count(fp);
+    if(c=0)
+      printf("File is empty.\nEnter some data.");
+      fclose(fp);
+      fp=fopen(f1,"w");
+      write(fp);
+}
