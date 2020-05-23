9fe72a5f168b12a459535fdd788bb3bf64d7abc2
SakivYadurh <58883626+SakivYadurh@users.noreply.github.com>
2020-05-23 16:36:27 +0530

    Create Searchtool.c

---
+++ Searchtool.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+char history[50][50]; 
+
+int main { 
+  int c,i=0; 
+  clrscr(); 
+  printf(“1. Search\n2. History\n Enter your choice:”);
+  scanf("%d",&c);
+  if(c==1){
+    
+  }
+  else if(c==2){
+    
+  }
+} 
