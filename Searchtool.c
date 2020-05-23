07d9c2f41abdd474d986e90cf359af071388e005
roshankumar2303 <58880369+roshankumar2303@users.noreply.github.com>
2020-05-23 17:00:18 +0530

    Update Searchtool.c

--- Searchtool.c
+++ Searchtool.c
@@ -1,16 +1,58 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-char history[50][50]; 
+#include<stdio.h>
+#include<stdlib.h>
 
-int main { 
-  int c,i=0; 
-  clrscr(); 
-  printf(“1. Search\n2. History\n Enter your choice:”);
-  scanf("%d",&c);
-  if(c==1){
-    
-  }
-  else if(c==2){
-    
-  }
-} 
+typedef struct node {
+    char[40] title;
+    struct node *next;
+} NODE;
+
+NODE *top = NULL;
+NODE *p;
+
+void push() {
+    p = (NODE *)malloc(sizeof(NODE));
+    printf("Enter the String : ");
+    scanf("%[^\n]%*c", &p -> title);
+    p -> next = top;
+    top = p;
+}
+
+void pop() {
+    p = top;
+    top = top -> next;
+    free(p);
+}
+
+void display() {
+    p = top;
+    while(p != NULL) {
+        printf("%s\n", p -> title);
+        p = p -> next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice;
+    printf("1 - Push\n2 - Pop\n3 - Display\nAny other number to EXIT\n");
+    do {
+        printf("\nEnter the choice : ");
+        scanf("%d", &choice);
+        if(top == NULL && (choice == 2 || choice == 3)) {
+            printf("Stack is empty\n");
+            continue;
+        }
+        switch(choice) {
+            case 1 : push();
+                     break;
+            case 2 : pop();
+                     break;
+            case 3 : display();
+                     break;
+            default : printf("Program Terminated\n");
+                      exit(0); 
+        }
+    }
+    while(choice > 0 && choice < 4);
+    return 0;
+}
