a0956c7a5a0de57d5d9fa842d92eddaaa10535dc
roshankumar2303 <58880369+roshankumar2303@users.noreply.github.com>
2020-05-23 22:09:52 +0530

    Update Searchtool.c

--- Searchtool.c
+++ Searchtool.c
@@ -44,39 +44,42 @@ void display() {
 // SEARCH OPERATIONS
 void searching(){
     char search[50];
-    printf("Search for:");
+    printf("Enter Statement: ");
     scanf("%s",search);
     push(search);
+    system("cls");
 }
 
 
 // HISTORY
 void history() {
     int choice = 0, t = 0;
-    if(top==NULL)
+    if(top==NULL){
         printf("No History\n");
+        system("pause");
+    }
     else{
         printf("Your History\n");
         display();
-        printf("1. Clear recent history\n2. Clear entire history\n3. Go back to main menu\nEnter your choice: ");
+        printf("1. Clear recent history\n2. Clear entire history\n3. Go back to main menu\n\nEnter your choice: ");
         scanf("%d",&choice);
         if(choice==1){
             pop();
-            printf("Recent History Cleared...\nPress any key to go back to main menu... \n");
-            t = getchar();
+            system("pause");
         }
         else if(choice==2){
             choice=0;
             while(1){
                 if(t==1){
-                    printf("Entire History Cleared...\nPress any key to go back to main menu... \n");
-                    t = getchar();
+                    printf("\nEntire History Cleared . . .\n");
+                    system("pause");
                     break;
                 }
                 t=pop();
             }
         }
         else if(choice==3){
+            system("cls");
             return;
         }
     }
@@ -85,13 +88,12 @@ void history() {
 
 int main() {
     int choice;
-    clrscr();
+    //clrscr();
     do {
         printf("MAIN MENU\n\n1. Search\n2. History\n\nAny other number to exit\n\nEnter your choice:");
         scanf("%d",&choice);
         system("cls"); // Console screen is CLEARED. i.e., Main screen instructions disappear and Search/History instructions appear
         if(choice==1){
-            choice=0;
             searching();
             /*Still to be continued*/
         }
