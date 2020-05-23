a00777ea0cf26d912aa01cc9e850ab58560aafa2
roshankumar2303 <58880369+roshankumar2303@users.noreply.github.com>
2020-05-23 20:35:23 +0530

    Update Searchtool.c
    Moved the code related to history part to a seperate function....
    And a few small changes...

--- Searchtool.c
+++ Searchtool.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Structure of nodes storing history of visited titles in a stack
 typedef struct hstr_node {
     char title[50];
     struct hstr_node *next;
@@ -9,6 +10,8 @@ typedef struct hstr_node {
 Hstr_List *top = NULL;
 Hstr_List *p;
 
+
+/******************** STACK OPERATIONS ********************/
 void push(char* s) {
     p = (Hstr_List *)malloc(sizeof(Hstr_List));
     p -> title=s;
@@ -35,51 +38,68 @@ void display() {
     }
     printf("\n");
 }
+/****************** STACK OPERATIONS END ******************/
+
+
+// SEARCH OPERATIONS
 void searching(){
     char search[50];
     scanf("%s",&search);
     push(search);
 }
 
-int main() {
-    int choice,t=0;
-    clrscr();
-    printf("1. Search\n2. History\nEnter your choice:");
-    scanf("%d",&choice);
-    if(choice==1){
-        choice=0;
-        searching();
-        /*Still to be continued*/
-    }
-    else if(choice==2){
-        choice=0;
-        if(top==NULL)
-            printf("No History\n");
-        else{
-            printf("Your History\n");
-            display();
-            printf("1. Clear history\n2. Search\nEnter your choice:");
-            scanf("%d",&choice);
-            if(choice==1){
-            choice=0;
-            printf("1. CLear recent history\n2.Clear entire history\nEnter your choice:");
-            scanf("%d",&choice);
-            if(choice==1)
-                pop();
-            else if(choice==2){
-                choice=0;
-                while(1){
-                    if(t==1){
-                        printf("Entire History Cleared\n")
-                        break();
-                    }
-                    t=pop();
-                }
-            }
+
+// HISTORY
+void history() {
+    int choice = 0, t = 0;
+    if(top==NULL)
+        printf("No History\n");
+    else{
+        printf("Your History\n");
+        display();
+        printf("1. Clear recent history\n2. Clear entire history\n3. Go back to main menu\nEnter your choice: ");
+        scanf("%d",&choice);
+        if(choice==1){
+            pop();
+            printf("Recent History Cleared...\nPress any key to go back to main menu... \n");
+            t = getchar();
         }
         else if(choice==2){
             choice=0;
-            searching();
+            while(1){
+                if(t==1){
+                    printf("Entire History Cleared...\nPress any key to go back to main menu... \n")
+                    t = getchar();
+                    break();
+                }
+                t=pop();
+            }
+        }
+        else if(choice==3){
+            return;
         }
     }
+    system("cls"); // Console screen is CLEARED. i.e., History instructions disappear and Main Menu instructions appear
+}
+
+int main() {
+    int choice;
+    clrscr();
+    do {
+        printf("MAIN MENU\n\n1. Search\n2. History\n\nAny other number to exit\n\nEnter your choice:");
+        scanf("%d",&choice);
+        system("cls"); // Console screen is CLEARED. i.e., Main screen instructions disappear and Search/History instructions appear
+        if(choice==1){
+            choice=0;
+            searching();
+            /*Still to be continued*/
+        }
+        else if(choice==2) {
+            history();
+        }
+        else{
+            printf("Exiting Application...\n")
+        }
+    } while(choice > 0 && choice <3);
+    return 0;
 }
