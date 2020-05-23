56b0680c3150c98d795b061f6fcec088b4a76f04
SakivYadurh <58883626+SakivYadurh@users.noreply.github.com>
2020-05-23 23:47:23 +0530

    Update Searchtool.c

--- Searchtool.c
+++ Searchtool.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct node {
+    int data;
+    struct node *left, *right;
+} NODE;
+NODE *root = NULL;
+
 // Structure of nodes storing history of visited titles in a stack
 typedef struct hstr_node {
     char* title;
@@ -40,14 +46,99 @@ void display() {
 }
 /****************** STACK OPERATIONS END ******************/
 
+NODE *bst_for_sorting(int words) {
+    int flag;
+    NODE *new, *current;
+        flag = 1;
+        current = root;
+        new = (NODE*)malloc(sizeof(NODE));
+        new -> left = NULL;
+        new -> right = NULL;
+        new -> data = words;
+        if(root == NULL) {
+            root = new;
+            continue;
+        }
+        while(flag) {
+            if(new -> data > current -> data) {
+                if(current -> right == NULL) {
+                    current -> right = new;
+                    flag = 0;
+                }
+                else
+                    current = current -> right;
+            }
+            else {
+                if(current -> left == NULL) {
+                    current -> left = new;
+                    flag = 0;
+                }
+                else
+                    current = current -> left;
+            }
+        }
+}
+void inorder(NODE *ptr) {
+    if(ptr -> left != NULL)
+        inorder(ptr -> left);
+    printf("%d ", ptr -> data);     /*Should change this to push the data into QUEUE*/
+    if(ptr -> right != NULL)
+        inorder(ptr -> right);
+}
+void count(FILE *fp){
+    int words=1;
+    char ch;
+    while((ch=getc(fp))!=EOF){
+        if(ch==' ')
+            words++;
+    }
+    bst_for_sorting(words);
+}
 
 // SEARCH OPERATIONS
 void searching(){
-    char search[50];
+    char search[50],ch;
+    int i=0,l,f=1;
     printf("Enter Statement: ");
     scanf("%s",search);
     push(search);
     system("cls");
+    l=strlen(search);
+    FILE *fp1,*fp2;
+    fp1=fopen("titles","r");
+    while((ch=getc(fp1))!=EOF){
+        if(ch==search[i]&&i<l)
+            i++;
+        else if(i==l){
+            if(f==1)
+                fp2=fopen("f1","r");
+            if(f==2)
+                fp2=fopen("f2","r");
+            if(f==3)
+                fp2=fopen("f3","r");
+            if(f==4)
+                fp2=fopen("f4","r"); 
+            if(f==5)
+                fp2=fopen("f5","r");
+            if(f==6)
+                fp2=fopen("f6","r");            
+            if(f==7)
+                fp2=fopen("f7","r");
+            if(f==8)
+                fp2=fopen("f8","r");
+            if(f==9)
+                fp2=fopen("f9","r");
+            if(f==10)
+                fp2=fopen("f10","r");
+            count(fp2);
+        }
+        else
+            i=0;
+        if(ch=='\n')
+            f++;
+    }
+    inorder(root);
+    /* TO BE CONTINUED */
 }
 
 
